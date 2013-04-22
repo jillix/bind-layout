5f49076f3aeeb34a2aa5945b595d6bf7339cabe5
Gabriel Petrovay <gabipetrovay@gmail.com>
2013-04-22 14:24:54 +0200

    Rewritten the module in the new client module format

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-layout
-======
+Mono Layout Module
+==================
 
-Layout module for bind
+This ia a layout module for the Mono web framework.
+
+Configuration
+-------------
+
+Besides the standard Mono module configuration options ('html', 'css', 'scripts'), the extra configuration options for this module are:
+
+```
+{
+    "title": "Test Title",
+
+    "modules": {
+        "domElemId1": "miid1",
+        "domElemId2": ["miid2", "alternativeMiid", "errorMiid"]
+    },
+
+    "binds": [BIND_OBJECT]
+}
+```
+
+where:
+
+  * **title**: the title that this layout module will give to the document
+  * **modules**: an object containing DOM element IDs as keys and the corresponding `miid` as values. If the value is an array, the layout will try to load the modules in order until one is successfull. This can be the case when the user is not yet logged in and instead of a module, a login or error module will be displayed.
+  * **bins**: an array of bind objects as described by the [Bind module](https://github.com/adioo/bind)
+
--- main.js
+++ main.js
@@ -1,103 +1,89 @@
-/*
-config = {
-    
-    "title": "Test Title",
+'use strict';
 
-    "modules": {
-        "domElemId1": "miid1",
-        "domElemId2": ["miid2", "alternativeMiid", "errorMiid"]
-    },
+var Bind = require('github/adioo/bind');
 
-    "binds": [BIND_OBJECT]
-}
-*/
-"use strict";
-
-define(["github/adioo/bind/v0.2.5/bind"], function(Bind) {
-
-    // a recursive function until a module
-    function tryNextModule(miids, index, container) {
-        // stop when no more modules
-        if (!miids[index]) {
-            return;
+// a recursive function until a module
+function tryNextModule (miids, index, container) {
+    // stop when no more modules
+    if (!miids[index]) {
+        return;
+    }
+    // otherwise go and try to load the module
+    M(container, miids[index], function(err, module) {
+        if (err) {
+            tryNextModule(miids, ++index, container);
         }
-        // otherwise go and try to load the module
-        M(container, miids[index], function(err, module) {
-            if (err) {
-                tryNextModule(miids, ++index, container);
+    });
+}
+
+module.exports = function (config) {
+
+    var target = this && this.dom ? this.dom : document;
+
+    // load css
+    if (config.css) {
+
+        // get head reference
+        var head = document.getElementsByTagName('head')[0];
+
+        for (var i in config.css) {
+
+            var href;
+
+            if (config.css[i].indexOf('http') > -1) {
+
+                href = config.css[i];
             }
-        });
+            else {
+
+                href = '/' + this.ok + '/core/getFile' + (config.css[i][0] == '/' ? '' : '/') + config.css[i]
+            }
+
+            // create link and append it to the DOM
+            var link = document.createElement('link');
+            var attributes = {
+                    rel:    'stylesheet',
+                    type:   'text/css',
+                    href:   href
+                };
+
+            for (var name in attributes) {
+
+                link.setAttribute(name, attributes[name]);
+            }
+
+            head.appendChild(link);
+        }
     }
 
-    return function(config) {
-        
-        var target = this && this.dom ? this.dom : document;
-        
-        // load css
-        if (config.css) {
-            
-            // get head reference
-            var head = document.getElementsByTagName("head")[0];
-            
-            for (var i in config.css) {
-                
-                var href;
-                
-                if (config.css[i].indexOf("http") > -1) {
-                    
-                    href = config.css[i];
-                }
-                else {
-                    
-                    href = "/" + this.ok + "/core/getFile" + (config.css[i][0] == "/" ? "" : "/") + config.css[i]
-                }
-                
-                // create link and append it to the DOM
-                var link = document.createElement("link");
-                var attributes = {
-                        rel:    "stylesheet",
-                        type:   "text/css",
-                        href:   href
-                    };
-                    
-                for (var name in attributes) {
-                    
-                    link.setAttribute(name, attributes[name]);
-                }
-                
-                head.appendChild(link);
-            }
-        }
-        
-        // load modules
-        for (var selector in config.modules) {
+    // load modules
+    for (var selector in config.modules) {
 
-            var modules = config.modules[selector];
-            var container = target.querySelector("#" + selector);
+        var modules = config.modules[selector];
+        var container = target.querySelector('#' + selector);
 
-            if (typeof modules === "string") {
-                M(container, modules);
-            }
-            // assuming an array
-            else if (modules.length) {
-                // start finding the first accessible miid in the module array
-                tryNextModule(modules, 0, container);
-            }
+        if (typeof modules === 'string') {
+            M(container, modules);
         }
-        
-        // set document title
-        if (config.title) {
-            document.title = config.title;
+        // assuming an array
+        else if (modules.length) {
+            // start finding the first accessible miid in the module array
+            tryNextModule(modules, 0, container);
         }
+    }
 
-        // run the binds
-        for (var i in config.binds) {
-            Bind.call(self, config.binds[i]);
-        }
-        
-        if (typeof this.onload === 'function') {
-            this.onload();
-        }
-    };
-});
+    // set document title
+    if (config.title) {
+        document.title = config.title;
+    }
+
+    // run the binds
+    for (var i in config.binds) {
+        Bind.call(self, config.binds[i]);
+    }
+
+    if (typeof this.onload === 'function') {
+        this.onload();
+    }
+};
 
