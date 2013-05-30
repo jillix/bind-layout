b53bead719629fa21dcc8b681d05a96dc9c2b91d
Ionica Bizau <bizauionica@yahoo.com>
2013-05-30 18:07:18 +0300

    Added reload function, and events.

--- layout.js
+++ layout.js
@@ -1,17 +1,18 @@
 'use strict';
 
 var Bind = require('github/jillix/bind');
+var Events = require('github/jillix/events');
 
 // a recursive function until a module
-function tryNextModule (miids, index, container) {
+function tryNextModule (miids, index, container, dataContext) {
     // stop when no more modules
     if (!miids[index]) {
         return;
     }
     // otherwise go and try to load the module
-    M(container, miids[index], function(err, module) {
+    M(container, miids[index], dataContext, function(err, module) {
         if (err) {
-            tryNextModule(miids, ++index, container);
+            tryNextModule(miids, ++index, container, dataContext);
         }
     });
 }
@@ -20,22 +21,32 @@ module.exports = function (config) {
 
     var self = this;
     var target = self && self.dom ? self.dom : document;
+    
+    Events.call(self, config);
 
-    // load modules
-    for (var selector in config.modules) {
+    function loadModules (dataCtx) {
+        // load modules
+        for (var selector in config.modules) {
 
-        var modules = config.modules[selector];
-        var container = target.querySelector('#' + selector);
+            var modules = config.modules[selector];
+            var container = target.querySelector('#' + selector);
 
-        if (typeof modules === 'string') {
-            M(container, modules);
-        }
-        // assuming an array
-        else if (modules.length) {
-            // start finding the first accessible miid in the module array
-            tryNextModule(modules, 0, container);
+            if (typeof modules === 'string') {
+                M(container, modules);
+            }
+            // assuming an array
+            else if (modules.length) {
+                // start finding the first accessible miid in the module array
+                tryNextModule(modules, 0, container, dataCtx);
+            }
         }
     }
+
+    loadModules(dataContext);
+
+    self.reload = function (dataCtx) {
+        loadModules(dataCtx);
+    };
 
     // set document title
     if (config.title) {
